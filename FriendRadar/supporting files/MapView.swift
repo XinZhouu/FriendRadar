ee02d6aa5809d5a5bbb7216328687abf9392650a
Xin Zhou <110301124+XinZhouu@users.noreply.github.com>
2024-05-02 15:47:29 -0700

    pin feature partially implemented
    worked

--- FriendRadar/supporting files/MapView.swift
+++ FriendRadar/supporting files/MapView.swift
@@ -20,42 +20,59 @@ struct MapView: View {
     var body: some View {
         //The initial postion must be passed
         //as a binding
-        Map (position: Binding<MapCameraPosition>(
-            get: { self.mapManager.location },
-            set: { self.mapManager.location = $0 }),
-             interactionModes: .all) {
-            // mark the current user location
-            Marker("", systemImage: "mappin", coordinate: mapManager.location.region?.center ?? CLLocationCoordinate2D(latitude: 37.8730, longitude: -122.2595))
-                .tint(.orange)
-                .tag(1)
-            
-            ForEach(locInfo) { property in
-                if let coordinate = UCBProperty.UCBProperties.first(where: { $0.name == property.name })?.coordinate{
-                    Annotation(property.name, coordinate: coordinate) {
-                        // customized annotation style
-                        AnnotationView(showFriendContent: $showFriendContent, targetLoc: property)
+        MapReader { proxy in
+            Map (position: Binding<MapCameraPosition>(
+                get: { self.mapManager.location },
+                set: { self.mapManager.location = $0 }),
+                 interactionModes: .all) {
+                // mark the current user location
+                Marker("", systemImage: "mappin", coordinate: mapManager.location.region?.center ?? CLLocationCoordinate2D(latitude: 37.8730, longitude: -122.2595))
+                    .tint(.orange)
+                    .tag(1)
+                
+                ForEach(locInfo) { property in
+                    if let coordinate = UCBProperty.UCBProperties.first(where: { $0.name == property.name })?.coordinate{
+                        Annotation(property.name, coordinate: coordinate) {
+                            // customized annotation style
+                            AnnotationView(showFriendContent: $showFriendContent, targetLoc: property)
+                        }
+                        // hide annotations on the map
+                        .annotationTitles(.hidden)
+                        .tag(2)
                     }
-                    // hide annotations on the map
-                    .annotationTitles(.hidden)
-                    .tag(2)
                 }
+                
+                //            UserAnnotation()
             }
-            
-//            UserAnnotation()
+                 .mapStyle(.hybrid(elevation: .realistic))
+                 .mapStyle(.imagery)
+                 .mapControls {
+                     //            MapUserLocationButton()
+                     MapCompass()
+                     MapScaleView()
+                     MapPitchToggle()
+                 }
+            //debugging
+                 .onMapCameraChange { context in
+                     print(context.region)
+                 }
+                 .animation(.easeInOut, value: mapManager.location)
+                 .safeAreaInset(edge: .bottom) {
+                     HStack {
+                         Spacer()
+                         Button(action:{}) {
+                             Image(systemName: "plus")
+                         }
+                         Spacer()
+                     }
+                     .background(.thinMaterial)
+                 }
+                 .onTapGesture { position in
+                     if let coordinate = proxy.convert(position, from: .local) {
+                         print(coordinate)
+                     }
+                 }
         }
-        .mapStyle(.hybrid(elevation: .realistic))
-        .mapStyle(.imagery)
-        .mapControls {
-//            MapUserLocationButton()
-            MapCompass()
-            MapScaleView()
-            MapPitchToggle()
-        }
-        //debugging
-        .onMapCameraChange { context in
-            print(context.region)
-        }
-        .animation(.easeInOut, value: mapManager.location)
     }
 }
 
